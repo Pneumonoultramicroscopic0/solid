8f6c73002278752ee1ae5f967ae2327c277bb508
pneumonoultramicroscpic0 <omar0madouni@gmail.com>
2024-05-21 03:24:46 +0100

    e02/ w e03/ rir zaydin i think ranjm3 tmarn c f e01/ daba nbdlo smito

---
+++ c/e01/ex12.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+    printf ("d5l A w B wn9olk wax nfs l2ixara :\n");
+    scanf ("%d%d", &a, &b);
+    c = a * b;
+    if (c>0){
+        printf ("A w B 3ndhom nfs l2ixara");
+        return 0;
+    }
+    else if (c<0){
+        printf ("A w B ma3ndhomx nfs l2ixara");
+        return 0;
+    }
+    else {
+        printf ("0 mo7ayid");
+    }
+    
+    return 0;
+}
